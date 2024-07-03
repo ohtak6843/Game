3bc4305c738dac9547edde37d59ec08b7ccfb66b
ohtak6843_Laptop <ohtak6843@gmail.com>
2024-07-04 02:52:15 +0900

    프로젝트 설정 끝

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,8 +1,10 @@
 ﻿// Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
+#include "pch.h"
 #include "framework.h"
 #include "Client.h"
+#include "Game.h"
 
 #define MAX_LOADSTRING 100
 
@@ -42,10 +44,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
+    unique_ptr<Game> game = make_unique<Game>();
+    game->Init();
+
     // 기본 메시지 루프입니다:
     while (true)
     {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
             if (msg.message == WM_QUIT) break;
 
             if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -54,6 +60,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                 DispatchMessage(&msg);
             }
         }
+
+        // TODO
+        game->Update();
     }
 
     return (int) msg.wParam;
@@ -80,7 +89,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CLIENT));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_CLIENT);
+    wcex.lpszMenuName   = nullptr;
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
